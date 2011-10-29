8feca6da7046d6db445a6731924bf61f06dc79a4
Markus Bachmann <info@bachi.biz>
2011-10-29 15:23:10 +0200

    Use doctrine classloader instead of closure

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,20 +1,13 @@
 <?php
-spl_autoload_register(function ($class) {
-	
-	if ( 0 !== strpos($class, 'Doctrine\\Search')) {
-		return false;
-	} 
-	
-	$path = __DIR__ . '/../lib';
-	$file = strtr($class, '\\', '/') . '.php';
-	$filename = $path . '/' . $file;
-	
-	if ( file_exists($filename) ) {
-		return (Boolean) require_once $filename;
-	}
-	
-	return false;
-} );
-
 require_once __DIR__ . '/../lib/vendor/Buzz/lib/Buzz/ClassLoader.php';
+require_once __DIR__ . '/../lib/vendor/doctrine-common/lib/Doctrine/Common/ClassLoader.php';
+
+// use statements
+use Doctrine\Common\ClassLoader;
+
+$loader = new ClassLoader('Doctrine\\Common', __DIR__ . '/../lib/vendor/doctrine-common');
+$loader->register();
+$loader = new ClassLoader('Doctrine\\Search', __DIR__ . '/../lib');
+$loader->register();
+
 Buzz\ClassLoader::register();
