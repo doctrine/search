49badcd287a840f362fcde683e5dfd516f229639
Markus Bachmann <info@bachi.biz>
2011-10-30 15:36:23 +0100

    Update response

--- lib/Doctrine/Search/Http/Response/Buzz.php
+++ lib/Doctrine/Search/Http/Response/Buzz.php
@@ -8,17 +8,42 @@ use Doctrine\Search\Http\Response;
 class Buzz implements Response
 {
     private $buzzResponse;
-    
+
+    /**
+     * @param \Buzz\Message\Response $response
+     */
     public function __construct(BuzzResponse $response)
     {
         $this->buzzResponse = $response;
     }
-    
+
+    /**
+     * @return int
+     */
+    public function getStatusCode()
+    {
+        return $this->buzzResponse->getStatusCode();
+    }
+
+    /**
+     * @return string
+     */
     public function getContent()
     {
         return $this->buzzResponse->getContent();
     }
-    
+
+    /**
+     * @return bool
+     */
+    public function isSuccessfull()
+    {
+        return 200 === $this->buzzResponse->getStatusCode();
+    }
+
+    /**
+     * @return string
+     */
     public function __toString()
     {
         return $this->buzzResponse->__toString();
