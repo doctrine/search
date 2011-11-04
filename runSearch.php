b28cc92b799566a00a74b625c0e0aeafc4c98bde
Mike Lohmann <mike.h.lohmann@googlemail.com>
2011-11-04 08:36:06 +0100

    added ElasticSearch Client

--- runSearch.php
+++ runSearch.php
@@ -1,10 +1,11 @@
 <?php
+
+require_once('./tests/bootstrap.php');
+
 use Doctrine\Search\SearchManager;
 use Doctrine\Search\Solr\Client as SearchClient;
 use Doctrine\Search\Solr\Connection;
 use Doctrine\ODM\MongoDB\DocumentManager;
-
-
 
 $connection = new Connection();
 $configuration = new Configuration('localhost', '8888', 'testindex' );
