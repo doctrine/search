a55c664fcf177b3a1a8e3382a16b18618e815af6
Markus Bachmann <markus.bachmann@digital-connect.de>
2011-11-08 17:01:34 +0100

    Add testcase for versionclass

--- lib/Doctrine/Search/Version.php
+++ lib/Doctrine/Search/Version.php
@@ -6,18 +6,19 @@ final class Version {
     /**
      * @var string
      */
-    static $version = '0.1 alpha';
+    static $version = '0.1-alpha';
 
     /**
      * Compare a given version with the current version
      *
      * @param string $version
      * @param string $operator
-     * @return mixed
+     * @return int Return -1 if it older, 0 if it the same, 1 if $version is newer
      */
-    static public function compare($version, $operator = '<')
+    static public function compare($version)
     {
-        return version_compare(static::$version, $version, $operator);
+
+        return version_compare($version, static::$version);
     }
 
 }
