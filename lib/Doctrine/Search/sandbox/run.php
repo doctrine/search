21a03604a92a8f399b6b32311fb866c30387d39e
Mike Lohmann <mike.h.lohmann@googlemail.com>
2011-11-17 10:02:25 +0100

    forgot to add run.php

---
+++ lib/Doctrine/Search/sandbox/run.php
@@ -0,0 +1,47 @@
+<?php
+require_once __DIR__ . '/bootstrap.php';
+
+use Doctrine\Search\SearchManager;
+use Doctrine\ODM\MongoDB\DocumentManager;
+use Doctrine\ODM\MongoDB\Configuration;
+use Doctrine\ODM\MongoDB\Mapping\ClassMetadata;
+use Doctrine\ODM\MongoDB\Mapping\Driver\AnnotationDriver;
+use Doctrine\Common\Annotations\AnnotationReader;
+use Doctrine\MongoDB\Connection;
+use Doctrine\Search\Mapping\Driver\AnnotationDriver as SearchAnnotationDriver;
+
+$config = new Configuration();
+
+$config->setProxyDir(__DIR__ . '/tmp/cache/Proxies');
+$config->setProxyNamespace('Proxies');
+
+$config->setHydratorDir(__DIR__ . '/tmp/cache/Hydrators');
+$config->setHydratorNamespace('Hydrators');
+
+$config->setDefaultDB('doctrine_odm_tests');
+
+$reader = new AnnotationReader();
+
+$documentsDir = __DIR__ . '/Documents';
+$annotationDriver = new AnnotationDriver($reader, $documentsDir);
+$config->setMetadataDriverImpl($annotationDriver);
+
+$conn = new Connection(null, array(), $config);
+$dm = DocumentManager::create($conn, $config);
+$uow = $dm->getUnitOfWork();
+
+$evm = $dm->getEventManager();
+$config = $dm->getConfiguration();
+$driver = $config->getMetadataDriverImpl();
+$paths = $driver->getPaths();
+
+$metaDataFactory = $dm->getMetadataFactory();
+
+
+
+$searchDriver = new SearchAnnotationDriver($reader, $paths);
+$evm->addEventListener('loadClassMetadata', $searchDriver);
+
+
+$metaDataFactory->getAllMetadata();
+var_dump($evm->hasListeners('loadClassMetadata'));
