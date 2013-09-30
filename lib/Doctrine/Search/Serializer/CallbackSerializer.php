331c15b8b26149af32d30a6afdb207ee9b36f204
MrHash <Hash@Hashs-MacBook-Pro.local>
2013-09-30 13:48:55 +0200

    Correction to callback serializer

--- lib/Doctrine/Search/Serializer/CallbackSerializer.php
+++ lib/Doctrine/Search/Serializer/CallbackSerializer.php
@@ -12,17 +12,18 @@ class CallbackSerializer implements SerializerInterface
     public function __construct($serializerCallback = 'toArray', $deserializerCallback = 'fromArray')
     {
         $this->serializerCallback = $serializerCallback;
+        $this->deserializerCallback = $deserializerCallback;
     }
 
     public function serialize($object)
     {
-        return $object->{$this->callback}();
+        return $object->{$this->serializerCallback}();
     }
     
     public function deserialize($entityName, $data)
     {
         $entity = new $entityName();
-        $entity->{$deserializerCallback}($data);
+        $entity->{$this->deserializerCallback}($data);
         return $entity;
     }
 }
